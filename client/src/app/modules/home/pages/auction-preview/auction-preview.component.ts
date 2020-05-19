2e2fd5af31231bb78643ec64a0e2aa95e7251484
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-05-20 01:00:54 +0200

    create auction preview component

--- client/src/app/modules/home/pages/auction-preview/auction-preview.component.ts
+++ client/src/app/modules/home/pages/auction-preview/auction-preview.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Route, Router} from '@angular/router';
+import {Auction} from '../../../../shared/models/auction';
+import {AuctionService} from '../../../../shared/services/auction.service';
+import {AttachmentService} from '../../../../shared/services/attachment.service';
 
 @Component({
   selector: 'app-auction-preview',
@@ -9,11 +12,36 @@ import {ActivatedRoute, Route, Router} from '@angular/router';
 export class AuctionPreviewComponent implements OnInit {
   auctionId: number;
   category: string;
-  constructor(private router: ActivatedRoute) { }
+  auction: Auction;
+  photosUrl: string[];
+
+  constructor(private router: ActivatedRoute, private auctionService: AuctionService, private attachmentService: AttachmentService) {
+  }
 
   ngOnInit(): void {
     this.auctionId = this.router.snapshot.queryParams.id;
     this.category = this.router.snapshot.queryParams.category;
+    this.loadAuction();
+    this.loadAuctionPhotos();
   }
 
+  private loadAuction() {
+    this.auctionService.getAuctionWithDetailsById(this.auctionId).subscribe(
+      res => {
+        this.auction = res;
+      },
+      err => {
+        alert('TODO');
+      });
+  }
+
+  private loadAuctionPhotos() {
+    this.attachmentService.getAuctionPhotosById(this.auctionId).subscribe(
+      res => {
+        this.photosUrl = res;
+      },
+      err => {
+        alert('TODO');
+      });
+  }
 }
