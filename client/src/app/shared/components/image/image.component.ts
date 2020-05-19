a08c86a39ef2fd4c578e1a32d0442afa96f28e3f
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-05-20 00:56:16 +0200

    create image component

--- client/src/app/shared/components/image/image.component.ts
+++ client/src/app/shared/components/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-image',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public path: string;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
