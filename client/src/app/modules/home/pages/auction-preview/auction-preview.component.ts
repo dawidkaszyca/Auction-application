2aa4f4612755bb395df630e1584a915c12fbb272
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-05-21 22:39:59 +0200

    Work with the preview component

--- client/src/app/modules/home/pages/auction-preview/auction-preview.component.ts
+++ client/src/app/modules/home/pages/auction-preview/auction-preview.component.ts
@@ -6,51 +6,51 @@ import {AttachmentService} from '../../../../shared/services/attachment.service'
 import {AuctionDetails} from '../../../../shared/models/auction-details';
 
 @Component({
-    selector: 'app-auction-preview',
-    templateUrl: './auction-preview.component.html',
-    styleUrls: ['./auction-preview.component.scss']
+  selector: 'app-auction-preview',
+  templateUrl: './auction-preview.component.html',
+  styleUrls: ['./auction-preview.component.scss']
 })
 export class AuctionPreviewComponent implements OnInit {
-    auctionId: number;
-    auctionDetails: AuctionDetails[];
-    category: string;
-    auction: Auction;
-    photosUrl: string[];
-    selected = 1;
+  auctionId: number;
+  auctionDetails: AuctionDetails[];
+  category: string;
+  auction: Auction;
+  photosUrl: Map<string, string>;
+  selected = 1;
 
-    constructor(private router: ActivatedRoute, private auctionService: AuctionService, private attachmentService: AttachmentService) {
-    }
+  constructor(private router: ActivatedRoute, private auctionService: AuctionService, private attachmentService: AttachmentService) {
+  }
 
-    ngOnInit(): void {
-        this.auctionId = this.router.snapshot.queryParams.id;
-        this.category = this.router.snapshot.queryParams.category;
-        this.loadAuction();
-        this.loadAuctionPhotos();
-    }
+  ngOnInit(): void {
+    this.auctionId = this.router.snapshot.queryParams.id;
+    this.category = this.router.snapshot.queryParams.category;
+    this.loadAuction();
+    this.loadAuctionPhotos();
+  }
 
-    private loadAuction() {
-        this.auctionService.getAuctionWithDetailsById(this.auctionId).subscribe(
-            res => {
-                this.auction = res;
-                this.auctionDetails = this.auction.auctionDetails;
-                this.auction.description = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
-            },
-            err => {
-                alert('TODO');
-            });
-    }
+  private loadAuction() {
+    this.auctionService.getAuctionWithDetailsById(this.auctionId).subscribe(
+      res => {
+        this.auction = res;
+        this.auctionDetails = this.auction.auctionDetails;
+        this.auction.description = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+        },
+      err => {
+        alert('TODO');
+      });
+  }
 
-    private loadAuctionPhotos() {
-        this.attachmentService.getAuctionPhotosById(this.auctionId).subscribe(
-            res => {
-                this.photosUrl = res;
-            },
-            err => {
-                alert('TODO');
-            });
-    }
+  private loadAuctionPhotos() {
+    this.attachmentService.getAuctionPhotosById(this.auctionId).subscribe(
+      res => {
+        this.photosUrl = new Map(Object.entries(res));
+      },
+      err => {
+        alert('TODO');
+      });
+  }
 
-    select(id: number) {
-        this.selected = id;
-    }
+  select(id: number) {
+    this.selected = id;
+  }
 }
