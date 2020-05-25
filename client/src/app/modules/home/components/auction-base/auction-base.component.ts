b3f0a50b9b28104afe79433f95da83df677558aa
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-05-26 01:09:23 +0200

    create second auction look on grid component

---
+++ client/src/app/modules/home/components/auction-base/auction-base.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {AuctionBaseField} from '../../../../shared/models/auction-base-field';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-auction-base',
+  templateUrl: './auction-base.component.html',
+  styleUrls: ['./auction-base.component.scss']
+})
+export class AuctionBaseComponent implements OnInit {
+
+  @Input()
+  auction: AuctionBaseField;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  openAuctionPage() {
+    this.router.navigate(['auction'],
+      {queryParams: {'title': this.auction.title, 'category': this.auction.category, 'id': this.auction.id}});
+  }
+}
