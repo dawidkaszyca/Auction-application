c2a19481d381fb328d7c19698ed1bb2c1c269096
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-06-02 21:40:11 +0200

    implement chat main-component

--- client/src/app/modules/home/pages/user-message/user-message.component.ts
+++ client/src/app/modules/home/pages/user-message/user-message.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {WebsocketService} from '../../../../shared/services/web-socket.service';
+import {NavigationService} from '../../../../core/header/navigation/navigation.service';
 
 @Component({
   selector: 'app-user-message',
   templateUrl: './user-message.component.html',
   styleUrls: ['./user-message.component.scss']
 })
-export class UserMessageComponent implements OnInit {
+export class UserMessageComponent implements OnInit, OnDestroy {
 
-  constructor(private webSocket: WebsocketService) { }
+  constructor(private webSocket: WebsocketService, private navigationService: NavigationService) { }
+
 
   ngOnInit(): void {
     this.webSocket._connect();
+    this.navigationService.show = false;
+  }
+
+  ngOnDestroy(): void {
+    this.navigationService.show = true;
   }
 
   send() {
