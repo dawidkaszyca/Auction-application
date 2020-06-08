d310d6aad28d3e5404af66fc7d19a21dbee42a25
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-06-08 19:00:00 +0200

    add google map preview component

---
+++ client/src/app/shared/components/map-preview/map-preview.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {City} from '../../models/auction-base-field';
+import {google} from 'google-maps';
+
+@Component({
+  selector: 'app-map-preview',
+  templateUrl: './map-preview.component.html',
+  styleUrls: ['./map-preview.component.scss']
+})
+export class MapPreviewComponent implements OnInit {
+
+  mapElement;
+  @Input()
+  city: City;
+  map: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.initMap();
+  }
+
+  initMap() {
+    const coords = new google.maps.LatLng(Number(this.city.latitude), Number(this.city.longitude));
+    const mapOptions: google.maps.MapOptions = {
+      center: coords,
+      zoom: 11,
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.mapElement = document.getElementById('map');
+    this.map = new google.maps.Map(this.mapElement, mapOptions);
+    new google.maps.Marker({
+      map: this.map,
+      position: coords
+    });
+  }
+}
