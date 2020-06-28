a7f7dd4a1da4da0142ab95afd66499cafadec341
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-06-28 19:24:23 +0200

    create short pagination component without sort

---
+++ client/src/app/modules/home/components/pagination/pagination-short-bar/pagination-short-bar.component.ts
@@ -0,0 +1,67 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Pagination} from '../../../../../shared/models/pagination';
+import {AuctionService} from '../../../../../shared/services/auction.service';
+
+@Component({
+  selector: 'app-pagination-short-bar',
+  templateUrl: './pagination-short-bar.component.html',
+  styleUrls: ['./pagination-short-bar.component.scss']
+})
+export class PaginationShortBarComponent implements OnInit, OnChanges {
+  @Input()
+  numberOfAuctions: number;
+  amountOfPages: number;
+  pagination: Pagination;
+  pageSizeList: any;
+  inputValue: any;
+
+  constructor(private auctionService: AuctionService) {
+    this.pagination = new Pagination();
+  }
+
+  ngOnInit(): void {
+    this.loadMaps();
+    this.addPaginationListener();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.numberOfAuctions) {
+      this.setDataAfterChanges();
+    }
+  }
+
+  private addPaginationListener() {
+    this.auctionService.pagination.subscribe(data => {
+      this.pagination.page = data.page;
+      this.setDataAfterChanges();
+    });
+  }
+
+  public resetPage(): void {
+    this.pagination.page = 0;
+    this.setDataAfterChanges();
+  }
+
+  private setDataAfterChanges() {
+    this.amountOfPages = Math.ceil(this.numberOfAuctions / this.pagination.pageSize);
+    if (this.amountOfPages === 0) {
+      this.amountOfPages = 1;
+    }
+    this.inputValue = this.pagination.page + 1 + '/' + this.amountOfPages;
+  }
+
+  changeInput(value: number) {
+    this.pagination.page += value;
+    this.inputValue = this.pagination.page + 1 + '/' + this.amountOfPages;
+    this.auctionService.pagination.next(this.pagination);
+  }
+
+  private loadMaps() {
+    this.pageSizeList = [];
+    this.pageSizeList.push(10);
+    this.pageSizeList.push(15);
+    this.pageSizeList.push(20);
+    this.pageSizeList.push(25);
+    this.pageSizeList.push(30);
+  }
+}
