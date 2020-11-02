44ae782e3447936c5078f1ec15e3ed00df45e1da
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-11-02 20:16:35 +0100

    create change, reset, remind password endpoints

--- api/src/main/java/pl/dawid/kaszyca/controller/UserController.java
+++ api/src/main/java/pl/dawid/kaszyca/controller/UserController.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.*;
 import pl.dawid.kaszyca.dto.UserDTO;
 import pl.dawid.kaszyca.model.User;
 import pl.dawid.kaszyca.service.UserService;
+import pl.dawid.kaszyca.vm.ChangePasswordVM;
+import pl.dawid.kaszyca.vm.ResetPasswordVM;
 
-import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
 @Slf4j
 @CrossOrigin
-@RequestMapping("/api")
+@RequestMapping("/api/account")
 public class UserController {
 
     private UserService userService;
@@ -24,9 +25,9 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity registerAccount(@Valid @RequestBody UserDTO managedUserVM) {
+    public ResponseEntity registerAccount(@RequestHeader("Language") String language, @RequestBody UserDTO managedUserVM) {
         try {
-            userService.registerUser(managedUserVM, managedUserVM.getPassword());
+            userService.registerUser(managedUserVM, managedUserVM.getPassword(), language);
             return new ResponseEntity(HttpStatus.CREATED);
         } catch (Exception e) {
             log.error("Cannot create user");
@@ -38,7 +39,7 @@ public class UserController {
     public ResponseEntity activateAccount(@RequestParam(value = "key") String key) {
         try {
             Optional<User> user = userService.activateRegistration(key);
-            return user != null ? new ResponseEntity(user, HttpStatus.OK) : new ResponseEntity(HttpStatus.NO_CONTENT);
+            return user.isPresent() ? new ResponseEntity(HttpStatus.OK) : new ResponseEntity(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             log.error("Cannot find user");
             return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(500));
@@ -60,6 +61,52 @@ public class UserController {
     public ResponseEntity updateUser(@RequestBody UserDTO user) {
         try {
             userService.updateUser(user.getFirstName(), user.getLastName(), user.getEmail());
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Cannot change user data");
+            return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @DeleteMapping("/password/{email}")
+    public ResponseEntity remindPassword(@RequestHeader("Language") String language, @PathVariable String email) {
+        try {
+            return userService.remindPassword(email, language) ?
+                    new ResponseEntity(HttpStatus.OK) : new ResponseEntity(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log.error("Cannot change user data");
+            return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @GetMapping("/check-reset-key/{key}")
+    public ResponseEntity checkResetKey(@PathVariable String key) {
+        try {
+            return userService.checkResetKey(key) ?
+                    new ResponseEntity(HttpStatus.OK) : new ResponseEntity(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log.error("Cannot change user data");
+            return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @PostMapping("/password")
+    public ResponseEntity resetPassword(@RequestHeader("Language") String language,
+                                        @RequestBody ResetPasswordVM resetPasswordVM) {
+        try {
+            userService.resetPassword(resetPasswordVM, language);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Cannot change user data");
+            return new ResponseEntity(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @PutMapping("/password")
+    public ResponseEntity updatePassword(@RequestHeader("Language") String language,
+                                         @RequestBody ChangePasswordVM changePasswordVM) {
+        try {
+            userService.updatePassword(changePasswordVM, language);
             return new ResponseEntity(HttpStatus.OK);
         } catch (Exception e) {
             log.error("Cannot change user data");
