7ec9fa8f0d7ed49a7cd7cb99c0dd99f88289beac
dawid.kaszyca <dawid.kaszyca3@gmail.com>
2020-11-02 20:13:37 +0100

    add mail templates to mail service

--- api/src/main/java/pl/dawid/kaszyca/service/MailService.java
+++ api/src/main/java/pl/dawid/kaszyca/service/MailService.java
@@ -1,13 +1,24 @@
 package pl.dawid.kaszyca.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import pl.dawid.kaszyca.model.User;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -16,6 +27,17 @@ import java.util.Properties;
 public class MailService {
 
     private JavaMailSenderImpl mailSender;
+
+    private static final String USER = "user";
+
+    private static final String BASE_URL = "baseUrl";
+
+    private static final String CURRENT_DATE = "currentDate";
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+    @Autowired
+    private MessageSource messageSource;
 
     public MailService(Environment environment) {
         mailSender = new JavaMailSenderImpl();
@@ -31,26 +53,57 @@ public class MailService {
     }
 
     @Async
-    public void sendMail(String to, String content, String subject) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(content);
-            message.setFrom(Objects.requireNonNull(mailSender.getUsername()));
-            this.mailSender.send(message);
-            log.debug("Sent email to User '{}'", to);
-        } catch (MailException e) {
-            log.warn("Email could not be sent to user '{}'", to, e);
-        }
+    public void sendActivationEmail(User user, String language) {
+        log.debug("Sending activation email to '{}'", user.getEmail());
+        sendEmailFromTemplate(user, "mail/activationEmail", "email.activation.title", language);
     }
 
     @Async
-    public void sendActiveMail(String firstName, String activeUrl, String email) {
-        String prefixMessage = "Thanks ";
-        String suffixMessage = " for your registration at our service. Confirm your account by clicking here : http://localhost:4200/api/confirm-registration/";
-        String content = prefixMessage + firstName + suffixMessage + activeUrl;
-        String subject = "Confirm your registration at ...TODO...";
-        sendMail(email, content, subject);
+    public void sendPasswordResetMail(User user, String language) {
+        log.debug("Sending password reset email to '{}'", user.getEmail());
+        sendEmailFromTemplate(user, "mail/passwordResetEmail", "email.reset.title", language);
+    }
+
+    public void sendPasswordChangedMail(User user, String language) {
+        log.debug("Sending password changed email to '{}'", user.getEmail());
+        sendEmailFromTemplate(user, "mail/passwordChangedEmail", "email.password.change.title", language);
+    }
+
+    private void sendEmailFromTemplate(User user, String templateName, String titleKey, String language) {
+        if (user.getEmail() == null) {
+            log.debug("Email doesn't exist for user '{}'", user.getLogin());
+            return;
+        }
+        Locale locale = Locale.forLanguageTag(language);
+        Context context = new Context(locale);
+        context.setVariable(USER, user);
+        context.setVariable(CURRENT_DATE, getCurrentDate());
+        context.setVariable(BASE_URL, "http://192.168.1.5:4200");
+        String content = templateEngine.process(templateName, context);
+        String subject = messageSource.getMessage(titleKey, null, locale);
+        sendEmail(user.getEmail(), subject, content, false, true);
+    }
+
+    private String getCurrentDate() {
+        Date now = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return simpleDateFormat.format(now);
+    }
+
+    private void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        log.info("Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
+                isMultipart, isHtml, to, subject, content);
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
+            message.setTo(to);
+            message.setFrom(Objects.requireNonNull(mailSender.getUsername()));
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            mailSender.send(mimeMessage);
+            log.debug("Sent email to User '{}'", to);
+        } catch (MailException | MessagingException e) {
+            log.warn("Email could not be sent to user '{}'", to, e);
+        }
     }
 }
